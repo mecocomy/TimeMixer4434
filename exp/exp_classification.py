1227ed6b2014497b57c9c5eeb1b5f9bb998e8505
ZengMengyuan <144615152+mecocomy@users.noreply.github.com>
2024-12-09 19:40:39 +0800

    Update exp_classification.py

--- exp/exp_classification.py
+++ exp/exp_classification.py
@@ -139,7 +139,7 @@ class Exp_Classification(Exp_Basic):
             print("Epoch: {} cost time: {}".format(epoch + 1, time.time() - epoch_time))
             train_loss = np.average(train_loss)
             vali_loss, val_accuracy = self.vali(vali_data, vali_loader, criterion)
-            test_loss, test_accuracy = self.vali(test_data, test_loader, criterion)
+            test_loss, test_accuracy = self.test(test_data, test_loader, criterion)
 
             print(
                 "Epoch: {0}, Steps: {1} | Train Loss: {2:.3f} Vali Loss: {3:.3f} Vali Acc: {4:.3f} Test Loss: {5:.3f} Test Acc: {6:.3f}"
